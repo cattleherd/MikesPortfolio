0b1a5c6e49618b3889404ba44016b635fe22637a
Radwan Ahmed <radwan.hussein@protonmail.com>
2023-03-16 22:33:01 -0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-blue-200">
       <Component {...pageProps} />
-      <Analytics/>
+      <Analytics />
     </div>
   );
 }
