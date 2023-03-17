70042f3cb2ca36f96e3ba1c1cc02e98a78f038af
Radwan Ahmed <radwan.hussein@protonmail.com>
2023-03-16 22:29:12 -0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/tailwind.css";
 import "../styles/burger.css";
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-blue-200">
       <Component {...pageProps} />
+      <Analytics/>
     </div>
   );
 }
